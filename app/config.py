2420f88ae3e839d793ddf7f8bff5770cb9104a75
Mariusz Woloszyn <emsi@users.noreply.github.com>
2023-06-27 20:33:54 +0200

    add typing

--- app/config.py
+++ app/config.py
@@ -13,13 +13,13 @@ class Settings(BaseSettings):
     DATA: Path = CWD.parent / "data"
     DATABASE: str = "docsearch.faiss"
 
-    GPT_MODEL = "gpt-3.5-turbo"
-    TEMPERATURE = 0
+    GPT_MODEL: str = "gpt-3.5-turbo"
+    TEMPERATURE: int = 0
 
-    OPENAI_API_KEY = ""
-    TAGLINE = "Ask a question"
+    OPENAI_API_KEY: str = ""
+    TAGLINE: str = "Ask a question"
 
-    SYSTEM_PROMPT = (
+    SYSTEM_PROMPT: str = (
         "You are a helpful assistant that answers questions."
         " If you are unsure about the answer politely decline and offer an original"
         " joke instead. No cheesy jokes about scientists or computer please!"
@@ -27,9 +27,11 @@ class Settings(BaseSettings):
     )
 
     # number of documents retrieved from the database
-    TOP_K = 3
+    TOP_K: int = 3
 
-    BACKEND = "chat_gpt"
+    BACKEND: str = "chat_gpt"
+
+    MPT_MODEL: str = "mpt-7b-instruct"
 
     MPT_MODEL = "mpt-7b-instruct"
 
