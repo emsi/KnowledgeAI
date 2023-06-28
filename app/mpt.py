c9154b333dbe29d19e8ce4e7d126494de047fd79
Mariusz Woloszyn <emsi@users.noreply.github.com>
2023-06-28 17:12:45 +0200

    rewrite mpt with streaming response

--- app/mpt.py
+++ app/mpt.py
@@ -1,104 +1,78 @@
-import warnings
-from typing import Any, Dict, Tuple
-
+"""MosaicML MPT backend implementation."""
 import streamlit as st
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import transformers
 
 import chat
 from config import settings
 
-INSTRUCTION_KEY = "### Instruction:"
-RESPONSE_KEY = "### Response:"
-END_KEY = "### End"
-INTRO_BLURB = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
-PROMPT_FOR_GENERATION_FORMAT = """{intro}
-{instruction_key}
-{instruction}
-{response_key}
-""".format(
-    intro=INTRO_BLURB,
-    instruction_key=INSTRUCTION_KEY,
-    instruction="{instruction}",
-    response_key=RESPONSE_KEY,
-)
-
 
 # Define a function to load the large language model
-@st.cache_resource(show_spinner="Please wait. Loading ML model...")
-def load_large_language_model(
-    model_name, torch_dtype=None, trust_remote_code=None, use_auth_token=None, padding_side="left"
+@st.cache_resource(show_spinner=f"Please wait. Loading {settings.MODEL}...")
+def load_large_language_model_pipeline(
+    model_name,
+    *,
+    torch_dtype=None,
+    trust_remote_code=None,
+    use_auth_token=None,
+    padding_side="left",
+    task="text-generation",
+    device=None,
+    streamer=None,
 ):
     """Load the large language model and tokenizer."""
-    model = AutoModelForCausalLM.from_pretrained(
+
+    config = transformers.AutoConfig.from_pretrained(model_name, trust_remote_code=True)
+    # config.attn_config['attn_impl'] = 'triton'  # change this to use triton-based FlashAttention
+    device = device or torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    config.init_device = device
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(
         model_name,
+        config=config,
         torch_dtype=torch_dtype,
         trust_remote_code=trust_remote_code,
         use_auth_token=use_auth_token,
-        low_cpu_mem_usage=True,
+        # low_cpu_mem_usage=True,
     )
-    tokenizer = AutoTokenizer.from_pretrained(
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
         model_name,
         trust_remote_code=trust_remote_code,
         use_auth_token=use_auth_token,
         padding_side=padding_side,
     )
-    return model, tokenizer
+    # model.to(device=device, dtype=torch.bfloat16)
 
-
-class InstructionTextGenerationPipeline:
-    def __init__(
-        self,
-        model_name,
-        torch_dtype=torch.bfloat16,
-        trust_remote_code=True,
-        use_auth_token=None,
-    ) -> None:
-        self.model, self.tokenizer = load_large_language_model(
-            model_name,
-            torch_dtype=torch_dtype,
-            trust_remote_code=trust_remote_code,
-            use_auth_token=use_auth_token,
-            padding_side="left",
+    if streamer is None:
+        streamer = transformers.TextStreamer(
+            tokenizer,
+            skip_prompt=True,
+            eos_token_id=tokenizer.eos_token_id,
+            pad_token_id=tokenizer.pad_token_id,
         )
-        if self.tokenizer.pad_token_id is None:
-            warnings.warn(
-                "pad_token_id is not set for the tokenizer. Using eos_token_id as pad_token_id."
-            )
-            self.tokenizer.pad_token = self.tokenizer.eos_token
+    else:
+        streamer = streamer(
+            tokenizer,
+            skip_prompt=True,
+            eos_token_id=tokenizer.eos_token_id,
+            pad_token_id=tokenizer.pad_token_id,
+        )
 
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        # self.model.eval()
-        self.model.to(device=device, dtype=torch_dtype)
+    return transformers.pipeline(
+        task,
+        model=model,
+        tokenizer=tokenizer,
+        device=device,
+        streamer=streamer,
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.pad_token_id,
+    )
 
-        self.generate_kwargs = {
-            "temperature": 0.1,
-            "top_p": 0.92,
-            "top_k": 0,
-            "max_new_tokens": 1024,
-            "use_cache": True,
-            "do_sample": True,
-            "eos_token_id": self.tokenizer.eos_token_id,
-            "pad_token_id": self.tokenizer.pad_token_id,
-            "repetition_penalty": 1.1,  # 1.0 means no penalty, > 1.0 means penalty, 1.2 from CTRL paper
-        }
 
-    def format_instruction(self, instruction):
-        return PROMPT_FOR_GENERATION_FORMAT.format(instruction=instruction)
-
-    def __call__(
-        self, instruction: str, **generate_kwargs: Dict[str, Any]
-    ) -> Tuple[str, str, float]:
-        s = PROMPT_FOR_GENERATION_FORMAT.format(instruction=instruction)
-        input_ids = self.tokenizer(s, return_tensors="pt").input_ids
-        input_ids = input_ids.to(self.model.device)
-        gkw = {**self.generate_kwargs, **generate_kwargs}
-        with torch.no_grad():
-            output_ids = self.model.generate(input_ids, **gkw)
-        # Slice the output_ids tensor to get only new tokens
-        new_tokens = output_ids[0, len(input_ids[0]) :]
-        output_text = self.tokenizer.decode(new_tokens, skip_special_tokens=True)
-        return output_text
+def format_prompt(instruction):
+    """Format the prompt for instruct text generation."""
+    template = "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n###Instruction\n{instruction}\n\n### Response\n"  # noqa
+    return template.format(instruction=instruction)
 
 
 class Chat(chat.Chat):
@@ -107,11 +81,23 @@ class Chat(chat.Chat):
     def __init__(self, stream_container):
         self.response = ""
         self.stream_container = stream_container
+
+        class StreamlitTextStreamer(transformers.TextStreamer):
+            chat = self
+
+            def on_finalized_text(self, text: str, stream_end: bool = False):
+                """Run on finalized text. Only available when streaming is enabled."""
+                self.chat.response += text
+                if stream_end:
+                    self.chat.response += "\n"
+                stream_container.markdown(self.chat.response)
+
         # Initialize the model and tokenizer
-        self.pipeline = InstructionTextGenerationPipeline(
+        self.pipeline = load_large_language_model_pipeline(
             settings.MODEL,
             torch_dtype=torch.bfloat16,
             trust_remote_code=True,
+            streamer=StreamlitTextStreamer,
         )
 
     def __call__(self, question, docsearch):
